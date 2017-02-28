c941739a321a6e61745d3042dbc2d3e75b32f1ab
Aneta Halz <aneta.halz@interia.pl>
2017-02-28 12:06:44 +0100

    correction

--- scripts.js
+++ scripts.js
@@ -1,19 +1,17 @@
-// plik scripts.js
-
 function getTriangleArea(a, h) {
-	if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane');
-	}
-	return a * h / 2
+  if (a <= 0 || h <= 0) {
+    return 'Nieprawidłowe dane'
+  }
+  return a * h / 2
 }
 
-console.log( getTriangleArea(10,6) )
+console.log( getTriangleArea(10,6) );
 
 var triangle1Area = getTriangleArea(10, 15);
-console.log( 'Pole pierwszego trójkąta ' + triangle1Area );
+console.log('Pole pierwszego trójkąta = ' + triangle1Area);
 
 var triangle2Area = getTriangleArea(13, 0);
-console.log( 'Pole drugiego trójkąta ' + triangle2Area );
+console.log('Pole drugiego trójkąta = ' + triangle2Area);
 
 var triangle3Area = getTriangleArea(100, 13.7);
-console.log( 'Pole trzeciego trójkąta ' + triangle3Area );
+console.log('Pole trzeciego trójkąta = ' + triangle3Area);
